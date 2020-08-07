760b6c837e4eba0add995c4173c8d00adc31a0c8
johnbrunningsatpraxent <john.brunnings@praxent.com>
2020-08-06 21:50:22 -0500

    Added initial Project.

---
+++ Praxent.API.Client.Mailchimp/Bootstrapper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Autofac;
+using Mandrill;
+using Microsoft.Extensions.Configuration;
+
+namespace Praxent.API.Client.Mailchimp
+{
+    public static class Bootstrapper
+    {
+        public static ContainerBuilder AddMailChimpClient(this ContainerBuilder builder, IConfiguration configuration)
+        {
+            try
+            {
+                var mandrillApiKey = configuration.GetValue<string>("MANDRILL_API_KEY");
+                builder.RegisterInstance(new MandrillApi(mandrillApiKey).Messages).As<IMandrillMessagesApi>();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            builder.RegisterType<MandrillApiClient>().As<IMandrillApiClient>();
+
+            return builder;
+        }
+    }
+}
---
+++ Praxent.API.Client.Mailchimp/MandrillApiClient.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Mandrill;
+using Mandrill.Model;
+
+namespace Praxent.API.Client.Mailchimp
+{
+    public class MandrillApiClient : IMandrillApiClient
+    {
+        private readonly IMandrillMessagesApi _mandrillMessagesApi;
+
+        public MandrillApiClient(IMandrillMessagesApi mandrillMessagesApi)
+        {
+            _mandrillMessagesApi = mandrillMessagesApi;
+        }
+
+        public async Task<IList<MandrillSendMessageResponse>> SendAsync(MandrillMessage message, bool async = false, string ipPool = null, DateTime? sendAtUtc = null)
+        {
+            return await _mandrillMessagesApi.SendAsync(message, async, ipPool, sendAtUtc);
+        }
+
+        public async Task<IList<MandrillSendMessageResponse>> SendTemplateAsync(MandrillMessage message, string templateName, IList<MandrillTemplateContent> templateContent = null, bool async = false,
+            string ipPool = null, DateTime? sendAtUtc = null)
+        {
+            return await _mandrillMessagesApi.SendTemplateAsync(message, templateName, templateContent, async, ipPool, sendAtUtc);
+        }
+
+        public async Task<IList<MandrillSendMessageResponse>> SendRawAsync(string rawMessage, string fromEmail = null, string fromName = null, IList<string> to = null,
+            bool? async = null, string ipPool = null, DateTime? sendAtUtc = null, string returnPathDomain = null)
+        {
+            return await _mandrillMessagesApi.SendRawAsync(rawMessage, fromEmail, fromName, to, async, ipPool, sendAtUtc, returnPathDomain);
+        }
+
+        public async Task<IList<MandrillMessageInfo>> SearchAsync(string query, DateTime? dateFrom = null, DateTime? dateTo = null, IList<string> tags = null,
+            IList<string> senders = null, IList<string> apiKeys = null, int? limit = null)
+        {
+            return await _mandrillMessagesApi.SearchAsync(query, dateFrom, dateTo, tags, senders, apiKeys, limit);
+        }
+
+        public async Task<IList<MandrillMessageTimeSeries>> SearchTimeSeriesAsync(string query, DateTime? dateFrom = null, DateTime? dateTo = null, IList<string> tags = null,
+            IList<string> senders = null)
+        {
+            return await _mandrillMessagesApi.SearchTimeSeriesAsync(query, dateFrom, dateTo, tags, senders);
+        }
+
+        public async Task<MandrillMessageInfo> InfoAsync(string id)
+        {
+            return await _mandrillMessagesApi.InfoAsync(id);
+        }
+
+        public async Task<MandrillMessageContent> ContentAsync(string id)
+        {
+            return await _mandrillMessagesApi.ContentAsync(id);
+        }
+
+        public async Task<MandrillMessage> ParseAsync(string rawMessage)
+        {
+            return await _mandrillMessagesApi.ParseAsync(rawMessage);
+        }
+
+        public async Task<IList<MandrillMessageScheduleInfo>> ListScheduledAsync(string to = null)
+        {
+            return await _mandrillMessagesApi.ListScheduledAsync(to);
+        }
+
+        public async Task<MandrillMessageScheduleInfo> RescheduleAsync(string id, DateTime sendAtUtc)
+        {
+            return await _mandrillMessagesApi.RescheduleAsync(id, sendAtUtc);
+        }
+
+        public async Task<MandrillMessageScheduleInfo> CancelScheduledAsync(string id)
+        {
+            return await _mandrillMessagesApi.CancelScheduledAsync(id);
+        }
+    }
+}
